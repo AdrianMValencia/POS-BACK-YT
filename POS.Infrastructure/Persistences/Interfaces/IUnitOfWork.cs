1c37548f5a4625ae25186c3b1568fa960fe3f67e
Adrian M Valencia <adrianmechatovalencia2@gmail.com>
2022-11-20 19:31:56 -0500

    first commit

---
+++ POS.Infrastructure/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using POS.Infrastructure.FileStorage;
+
+namespace POS.Infrastructure.Persistences.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //Declaración o matricula de nuestra interfaces a nivel de repository
+        ICategoryRepository Category { get; }
+        IUserRepository User { get; }
+        IAzureStorage Storage { get; }
+        IProviderRepository Provider { get; }
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
---
+++ POS.Infrastructure/Persistences/Repositories/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using POS.Infrastructure.FileStorage;
+using POS.Infrastructure.Persistences.Contexts;
+using POS.Infrastructure.Persistences.Interfaces;
+
+namespace POS.Infrastructure.Persistences.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly POSContext _context;
+        public ICategoryRepository Category { get; private set; }
+        public IUserRepository User { get; private set; }
+        public IAzureStorage Storage { get; private set; }
+        public IProviderRepository Provider { get; private set; }
+
+        public UnitOfWork(POSContext context, IConfiguration configuration)
+        {
+            _context = context;
+            Category = new CategoryRepository(_context);
+            User = new UserRepository(_context);
+            Storage = new AzureStorage(configuration);
+            Provider = new ProviderRepository(_context);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
